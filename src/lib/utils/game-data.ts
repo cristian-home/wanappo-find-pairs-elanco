efe5c122d107cf069d686151d16c847b6738ccf9
Cristian Home <cristian_david033@hotmail.com>
2025-07-08 21:09:00 -0500

    feat: add new tile images and update game data structure

--- src/lib/utils/game-data.ts
+++ src/lib/utils/game-data.ts
@@ -4,12 +4,12 @@ import image3 from '@/assets/img/tiles/3.webp'
 import image4 from '@/assets/img/tiles/4.webp'
 import image5 from '@/assets/img/tiles/5.webp'
 import image6 from '@/assets/img/tiles/6.webp'
-// import image7 from '@/assets/img/tiles/7.webp'
-// import image8 from '@/assets/img/tiles/8.webp'
-// import image9 from '@/assets/img/tiles/9.webp'
-// import image10 from '@/assets/img/tiles/10.webp'
-// import image11 from '@/assets/img/tiles/11.webp'
-// import image12 from '@/assets/img/tiles/12.webp'
+import image7 from '@/assets/img/tiles/7.webp'
+import image8 from '@/assets/img/tiles/8.webp'
+import image9 from '@/assets/img/tiles/9.webp'
+import image10 from '@/assets/img/tiles/10.webp'
+import image11 from '@/assets/img/tiles/11.webp'
+import image12 from '@/assets/img/tiles/12.webp'
 
 export interface RawTile {
   matchValue: number
@@ -95,76 +95,76 @@ export const tilesData: RawTile[] = [
     isRevealed: false,
     isMatched: false
   },
-  // {
-  //   matchValue: 7,
-  //   image: image7,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 7,
-  //   image: image7,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 8,
-  //   image: image8,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 8,
-  //   image: image8,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 9,
-  //   image: image9,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 9,
-  //   image: image9,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 10,
-  //   image: image10,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 10,
-  //   image: image10,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 11,
-  //   image: image11,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 11,
-  //   image: image11,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 12,
-  //   image: image12,
-  //   isRevealed: false,
-  //   isMatched: false
-  // },
-  // {
-  //   matchValue: 12,
-  //   image: image12,
-  //   isRevealed: false,
-  //   isMatched: false
-  // }
+  {
+    matchValue: 7,
+    image: image7,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 7,
+    image: image7,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 8,
+    image: image8,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 8,
+    image: image8,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 9,
+    image: image9,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 9,
+    image: image9,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 10,
+    image: image10,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 10,
+    image: image10,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 11,
+    image: image11,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 11,
+    image: image11,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 12,
+    image: image12,
+    isRevealed: false,
+    isMatched: false
+  },
+  {
+    matchValue: 12,
+    image: image12,
+    isRevealed: false,
+    isMatched: false
+  }
 ]
