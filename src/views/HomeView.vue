d759540682bf0c29eecd1664ed9459f18f6a5e4d
Cristian Home <cristian_david033@hotmail.com>
2025-07-10 10:38:32 -0500

    feat: swap CADLogo with ElancoLogo and adjust styling in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -101,7 +101,7 @@ onMounted(() => {
       backgroundPosition: 'center 75%'
     }"
   >
-    <ElancoLogo class="absolute top-4 right-4 w-32 h-16" ref="elancoLogoRef" />
+    <CADLogo class="absolute top-4 right-4 h-16" ref="elancoLogoRef" />
     <div
       ref="pageContent"
       class="w-full h-full overflow-x-hidden overflow-y-hidden grid grid-cols-1 grid-rows-[auto_minmax(0,1fr)_auto] justify-items-center align-items-center gap-4 p-4"
@@ -114,8 +114,8 @@ onMounted(() => {
         );
       "
     >
-      <div class="col-start-1 col-end-1 row-start-1 row-end-1 mt-16">
-        <CADLogo class="h-64 w-auto drop-shadow-xs drop-shadow-white" />
+      <div class="col-start-1 col-end-1 row-start-1 row-end-1 mt-0">
+        <ElancoLogo class="h-32 w-auto drop-shadow-xs drop-shadow-white" />
       </div>
       <div
         :leave="{ opacity: 0 }"
