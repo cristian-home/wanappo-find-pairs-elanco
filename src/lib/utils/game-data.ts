50b026944aa826457003ff5dafc89b1c513eb862
Cristian Home <cristian_david033@hotmail.com>
2025-07-11 11:17:28 -0500

    refactor: remove unused tile images and update game data

--- src/lib/utils/game-data.ts
+++ src/lib/utils/game-data.ts
@@ -4,13 +4,6 @@ import image3 from '@/assets/img/tiles/3.webp'
 import image4 from '@/assets/img/tiles/4.webp'
 import image5 from '@/assets/img/tiles/5.webp'
 import image6 from '@/assets/img/tiles/6.webp'
-import image7 from '@/assets/img/tiles/7.webp'
-import image8 from '@/assets/img/tiles/8.webp'
-import image9 from '@/assets/img/tiles/9.webp'
-import image10 from '@/assets/img/tiles/10.webp'
-import image11 from '@/assets/img/tiles/11.webp'
-import image12 from '@/assets/img/tiles/12.webp'
-import image13 from '@/assets/img/tiles/13.webp'
 
 export interface RawTile {
   matchValue: number
@@ -93,90 +86,6 @@ export const tilesData: RawTile[] = [
   {
     matchValue: 6,
     image: image6,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 7,
-    image: image7,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 7,
-    image: image7,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 8,
-    image: image8,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 8,
-    image: image8,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 9,
-    image: image9,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 9,
-    image: image9,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 10,
-    image: image10,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 10,
-    image: image10,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 11,
-    image: image11,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 11,
-    image: image11,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 12,
-    image: image12,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 12,
-    image: image12,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 13,
-    image: image13,
-    isRevealed: false,
-    isMatched: false
-  },
-  {
-    matchValue: 13,
-    image: image13,
     isRevealed: false,
     isMatched: false
   }
